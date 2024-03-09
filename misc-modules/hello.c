ef549e8d03bfa1b7bb2ab4d9b7e8ae8c5c18f404
SherifOmarAbdelaziz <sherif.omar.abdelaziz.2013@gmail.com>
2024-03-09 20:36:31 +0200

    Update hello.c with username

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -8,12 +8,14 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int hello_init(void)
 {
 	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, SherifOmarAbdelaziz\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
 	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, SherifOmarAbdelaziz\n");
 }
 
 module_init(hello_init);
